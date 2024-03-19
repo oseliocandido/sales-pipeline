d7090e814397e28216ba4f6d7009bdfe244e3f50
oselio <oseliocandido@outlook.com>
2024-03-19 22:51:47 +0000

    refactor: Removed error message

--- tasks/s3/client.py
+++ tasks/s3/client.py
@@ -3,6 +3,8 @@ from dotenv import dotenv_values
 from botocore.exceptions import ClientError
 import boto3
 from pathlib import PosixPath
+import streamlit as st
+
 
 config = dotenv_values()
 
@@ -14,6 +16,7 @@ def upload_s3(file_name: PosixPath, bucket: str, key: str) -> bool:
         aws_access_key_id=config["AWS_ACCESS_KEY"],
         aws_secret_access_key=config["AWS_SECRET_ACCESS_KEY"],
     )
+
     try:
         s3_client.upload_file(Filename=file_name, Bucket=bucket, Key=key)
     except ClientError:
@@ -31,6 +34,5 @@ def download_s3(bucket: str, key: str) -> BytesIO:
         s3_io_data = BytesIO()
         s3_client.download_fileobj(bucket, key, s3_io_data)
     except ClientError as e:
-        print(f"Error: {str(e)}")
         return False
     return s3_io_data
