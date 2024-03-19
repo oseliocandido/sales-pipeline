3bbfd0df8afada5b1aa33040f7a234fd774989c4
oselio <oseliocandido@outlook.com>
2024-03-19 11:41:55 +0000

    refactor: Moved print of error messages to another function

--- tasks/s3/client.py
+++ tasks/s3/client.py
@@ -2,11 +2,13 @@ from io import BytesIO
 from dotenv import dotenv_values
 from botocore.exceptions import ClientError
 import boto3
+from pathlib import PosixPath
 
 config = dotenv_values()
 
 
-def upload_s3(file_name, bucket: str, key: str) -> bool:
+# Botar dps pra updr parquet e ler com duckdb
+def upload_s3(file_name: PosixPath, bucket: str, key: str) -> bool:
     s3_client = boto3.client(
         "s3",
         aws_access_key_id=config["AWS_ACCESS_KEY"],
@@ -14,8 +16,7 @@ def upload_s3(file_name, bucket: str, key: str) -> bool:
     )
     try:
         s3_client.upload_file(Filename=file_name, Bucket=bucket, Key=key)
-    except ClientError as e:
-        print(f"Error: {str(e)}")
+    except ClientError:
         return False
     return True
 
