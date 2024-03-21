2f4a9229a49d817089d3d30e6969f290a7d12e2e
oselio <oseliocandido@outlook.com>
2024-03-21 20:05:39 +0000

    refactor: Changed to OOP approach

--- tasks/s3/client.py
+++ tasks/s3/client.py
@@ -1,38 +1,33 @@
 from io import BytesIO
 from dotenv import dotenv_values
 from botocore.exceptions import ClientError
-import boto3
 from pathlib import PosixPath
-import streamlit as st
-
+from boto3 import client
 
 config = dotenv_values()
 
 
-# Botar dps pra updr parquet e ler com duckdb
-def upload_s3(file_name: PosixPath, bucket: str, key: str) -> bool:
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=config["AWS_ACCESS_KEY"],
-        aws_secret_access_key=config["AWS_SECRET_ACCESS_KEY"],
-    )
+class S3Tasks:
+    def __init__(self):
+        self.config = dotenv_values()
+        self.s3_client = client(
+            "s3",
+            aws_access_key_id=self.config["AWS_ACCESS_KEY"],
+            aws_secret_access_key=self.config["AWS_SECRET_ACCESS_KEY"],
+        )
 
-    try:
-        s3_client.upload_file(Filename=file_name, Bucket=bucket, Key=key)
-    except ClientError:
-        return False
-    return True
+    # Botar dps pra updr parquet e ler com duckdb
+    def upload_s3(self, file_name: PosixPath, bucket: str, key: str) -> bool:
+        try:
+            self.s3_client.upload_file(Filename=file_name, Bucket=bucket, Key=key)
+        except ClientError:
+            return False
+        return True
 
-
-def download_s3(bucket: str, key: str) -> BytesIO:
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=config["AWS_ACCESS_KEY"],
-        aws_secret_access_key=config["AWS_SECRET_ACCESS_KEY"],
-    )
-    try:
+    def download_s3(self, bucket: str, key: str) -> BytesIO:
         s3_io_data = BytesIO()
-        s3_client.download_fileobj(bucket, key, s3_io_data)
-    except ClientError as e:
-        return False
-    return s3_io_data
+        try:
+            self.s3_client.download_fileobj(bucket, key, s3_io_data)
+        except ClientError as e:
+            return False
+        return s3_io_data
