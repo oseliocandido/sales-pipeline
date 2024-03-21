093775d990c82ece7996f9ef8089873f3b085314
oselio <oseliocandido@outlook.com>
2024-03-21 20:10:39 +0000

    feat: Added more classes from models module in import

--- validations/validate.py
+++ validations/validate.py
@@ -5,24 +5,35 @@ from typing import List
 from validations.models import Target
 from validations.models import Products
 from validations.models import SalesOrder
+from validations.models import SalesPerson
+from validations.models import Sales
 
+import streamlit as st
 
-CLASS_MAP = {"Target": Target, "Products": Products, "Sales": SalesOrder}
+CLASS_MAP = {
+    "Target": Target,
+    "Products": Products,
+    "Sales": Sales,
+    "SalesPerson": SalesPerson,
+    "SalesOrder": SalesOrder,
+}
 
 
 def validate_model(path: PosixPath) -> List[str]:
     errors = []
     target_class = CLASS_MAP[path.stem]
-
     # Remove columns after is loaded in database and do there some filtering
-    for index, row in pd.read_csv(path, delimiter="\t", engine="pyarrow").iterrows():
+    for _, row in pd.read_csv(
+        path, delimiter="\t", engine="pyarrow", quotechar='"'
+    ).iterrows():
         try:
             target_class(**row.to_dict())
-        except ValidationError as error_message:
-            errors.append(f"{error_message} line {index}")
+        except ValidationError:
+            errors.append(1)
+            break
     return errors
 
 
 # bota isso pro arquivo de logs mostrando a linhaa
 # for error in validation_errors:
-#                 st.error(e)
+# errors.append(f"{error_message} line {index}")
