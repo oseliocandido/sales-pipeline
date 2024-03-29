488273ce4b7afde8e38691744edbf569168cee9a
oselio <oseliocandido@outlook.com>
2024-03-29 20:41:42 +0000

    remove: Deleted files

--- src/actions/clean_idea_transformations.py
+++
@@ -1,7 +0,0 @@
-# TODO:
-
-## Products Table
-## Standard cost column name must be replace em empyt space bettworns with  char _
-## Trim in specific column os specifc table jmaybe this one
-
-## In Sales Table, it will make parse transformatio in the Target Month column after is ok in pydantic
